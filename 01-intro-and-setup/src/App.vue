7f5845c67e57667b00ce4355d82069ab952816de
danielOuattara <daniel.ouattara@gmx.com>
2023-01-26 20:27:54 +0100

    intro and setup boilerplate

---
+++ 01-intro-and-setup/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <h1>Vue 3 TypeScript</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+});
+</script>
+
+<style></style>
